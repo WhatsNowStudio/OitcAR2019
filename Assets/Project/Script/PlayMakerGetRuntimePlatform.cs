d57dae4084f391cd4c691b1fd661078299420d46
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2019-05-20 17:43:24 +0800

    更新

---
+++ Assets/Project/Script/PlayMakerGetRuntimePlatform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Device)]
+    [Tooltip("取得運行平台")]
+    public class PlayMakerGetRuntimePlatform : FsmStateAction
+    {
+
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        public FsmString storePlatform;
+
+
+
+        public override void OnEnter()
+        {
+            if (Application.platform == RuntimePlatform.Android)
+                storePlatform.Value = ("Android");
+            if (Application.platform == RuntimePlatform.WindowsPlayer)
+                storePlatform.Value = ("WinPlayer");
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
+                storePlatform.Value = ("ios");
+            if (Application.platform == RuntimePlatform.OSXEditor)
+                storePlatform.Value = ("osxedit");
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+                storePlatform.Value = ("Winedit");
+        Finish();
+        }
+
+
+    }
+}
